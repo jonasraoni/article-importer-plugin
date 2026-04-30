ec1cb0d3a331f04fab75fd53ff2ef46f5f1f4c34
Jonas Raoni Soares da Silva <github@raoni.org>
2026-04-30 18:25:21 +0200

    Added collab node as a fallback to the author's name

--- parsers/jats/AuthorParser.php
+++ parsers/jats/AuthorParser.php
@@ -57,11 +57,12 @@ trait AuthorParser
         $lastName = $this->selectText('surname', $node);
         $prefix = $this->selectText('prefix', $node);
         $suffix = $this->selectText('suffix', $node);
+        $collab = $this->selectFirst('collab', $authorNode);
         if ($lastName && !$firstName) {
             $firstName = $lastName;
             $lastName = '';
         } elseif (!$lastName && !$firstName) {
-            $firstName = $this->getConfiguration()->getContext()->getName($this->getLocale());
+            $firstName = $collab ?: $this->getConfiguration()->getContext()->getName($this->getLocale());
         }
         $firstName = ($prefix ? "{$prefix} " : '') . $firstName;
         $lastName = $lastName . ($suffix ? " {$suffix}" : '');
