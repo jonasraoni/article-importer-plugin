da0ebd43de87c27022b87be27c70091b4e8dacb1
Jonas Raoni Soares da Silva <github@raoni.org>
2026-05-04 17:48:07 +0200

    #19 Fix XPath for ORCID selection in AuthorParser

--- parsers/jats/AuthorParser.inc.php
+++ parsers/jats/AuthorParser.inc.php
@@ -61,7 +61,7 @@ trait AuthorParser
         $firstName = ($prefix ? "{$prefix} " : '') . $firstName;
         $lastName = $lastName . ($suffix ? " {$suffix}" : '');
         $email = $this->selectText('email', $authorNode);
-        $orcid = $this->selectText("//uri[@content-type='orcid']", $authorNode);
+        $orcid = $this->selectText(".//uri[@content-type='orcid']", $authorNode);
         $affiliations = [];
         $biography = null;
 
