43b1260a00c5b764a6157c881c9135ee8d1e0545
Jonas Raoni Soares da Silva <github@raoni.org>
2026-05-19 23:23:25 +0200

    #35 Updates code to store the JATS file on the JATS tab

--- parsers/jats/PublicationParser.php
+++ parsers/jats/PublicationParser.php
@@ -160,7 +160,7 @@ trait PublicationParser
         $this->_insertPDFGalley($publication);
 
         // Store the JATS XML
-        $this->_insertXMLSubmissionFile();
+        $this->_insertXMLSubmissionFile($publication);
         // Process full text and generate HTML files
         $this->_processFullText(false);
         $this->_insertHTMLGalley($publication);
@@ -204,7 +204,7 @@ trait PublicationParser
     /**
      * Inserts the XML as a JATS file
      */
-    private function _insertXMLSubmissionFile(): void
+    private function _insertXMLSubmissionFile(Publication $publication): void
     {
         $file = $this->getArticleVersion()->getMetadataFile();
         $filename = $file->getPathname();
@@ -233,7 +233,8 @@ trait PublicationParser
         $newSubmissionFile->setData('createdAt', Core::getCurrentDate());
         $newSubmissionFile->setData('updatedAt', Core::getCurrentDate());
         $newSubmissionFile->setData('name', $file->getFilename(), $this->getLocale());
-
+        $newSubmissionFile->setData('assocType', Application::ASSOC_TYPE_PUBLICATION);
+        $newSubmissionFile->setData('assocId', $publication->getId());
         $submissionFileId = Repo::submissionFile()->add($newSubmissionFile);
 
         /** @var DOMElement $asset */
