abeb61795a566b03ce8664cc2fd9f36279420bdc
Jonas Raoni Soares da Silva <github@raoni.org>
2026-06-09 14:50:53 +0200

    #40 Setup the variant type
    (cherry picked from commit b136318127d7b2887f2cbcbd2810d4808efa66f4)

--- parsers/jats/PublicationParser.php
+++ parsers/jats/PublicationParser.php
@@ -31,6 +31,7 @@ use PKP\core\Core;
 use APP\core\Application;
 use PKP\i18n\LocaleConversion;
 use PKP\submission\Genre;
+use PKP\submissionFile\enums\MediaVariantType;
 use PKP\submissionFile\SubmissionFile;
 use APP\facades\Repo;
 use PKP\controlledVocab\ControlledVocab;
@@ -413,7 +414,7 @@ trait PublicationParser
         $newSubmissionFile->setData('credit', '');
         $newSubmissionFile->setData('copyrightOwner', '');
         $newSubmissionFile->setData('terms', '');
-
+        $newSubmissionFile->setData('variantType', MediaVariantType::WEB);
         Repo::submissionFile()->add($newSubmissionFile);
     }
 
